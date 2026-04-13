04b02a6b0dba78efeae6bdd2ea365e43968fc98f
Amir Pirzadeh <amir.pirzadeh@unicard-uk.com>
2026-04-13 17:15:58 +0100

    first commit

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import ForceGraph3D from "3d-force-graph";
+import "./style.css";
+
+// Random tree
+const N = 5_000;
+const gData = {
+  nodes: [...Array(N).keys()].map((i) => ({ id: i })),
+  links: [...Array(N).keys()]
+    .filter((id) => id)
+    .map((id) => ({
+      source: id,
+      target: Math.round(Math.random() * (id - 1)),
+    })),
+};
+
+const Graph = new ForceGraph3D(document.getElementById("3d-graph")!).graphData(
+  gData,
+);
